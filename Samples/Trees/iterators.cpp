78ce3690749a9ec0f180213aad979c0b68f72046
NickGeorgiev <nikikg95@gmail.com>
2018-11-29 19:32:28 +0200

    Added initial config for the range iterator of tree Added operator != and tree.end() method in order to be consistent with stl iterators. Old code is present for comparison. Also added two test for the two types of iterations.

--- Samples/Trees/iterators.cpp
+++ Samples/Trees/iterators.cpp
@@ -30,12 +30,18 @@ class LRoRIterator {
     void operator++(int=0);
     bool end()const;
     T operator*() const;
-
+    bool operator != (const LRoRIterator<T>&) const;
 };
 
 template<class T>
+LRoRIterator<T>::LRoRIterator(TreeNode<T>* root)  {
+    tasks.push(new Task<T>(root));
+    unwind();
+}
+
+template<class T>
 void LRoRIterator<T>::unwind() {
-    while(!tasks.empty() && !tasks.top()->toPrint){
+    while(!tasks.empty() && tasks.top()->node && !tasks.top()->toPrint){
         Task<T>* currentTask = tasks.top();
         tasks.pop();
         if (currentTask->node->right) {
@@ -50,14 +56,14 @@ void LRoRIterator<T>::unwind() {
 }
 
 template<class T>
-LRoRIterator<T>::LRoRIterator(TreeNode<T>* root)  {
-    tasks.push(new Task<T>(root));
-    unwind();
-}
-
-template<class T>
 bool LRoRIterator<T>::end() const {
     return tasks.empty();
+}
+
+//Alternative to call end() function
+template<class T>
+bool LRoRIterator<T>::operator != (const LRoRIterator<T>& other) const {
+    return !tasks.empty() && tasks.top() != other.tasks.top();
 }
 
 template<class T>
@@ -126,7 +132,7 @@ LRoRIterator<T> BTree<T>::begin()
 template<class T>
 LRoRIterator<T> BTree<T>::end()
 {
-	return LRoRIterator<T>(root,true);
+	return LRoRIterator<T>(nullptr);
 }
 
 template<class T>
@@ -134,6 +140,81 @@ void BTree<T>::addBOT(const T& elem) {
 	addHelp(root, elem);
 }
 
+bool TreeIterateFirstTest(){
+    std::cout<< "Iterating tree with end() method of the tree and operator !=: ";
+    BTree<int> tree;
+	tree.addBOT(10);
+	tree.addBOT(7);
+	tree.addBOT(4);
+	tree.addBOT(8);
+	tree.addBOT(3);
+	tree.addBOT(5);
+    tree.addBOT(14);
+    tree.addBOT(11);
+    tree.addBOT(16);
+    tree.addBOT(15);
+    tree.addBOT(18);
+    std::vector<int> expectedResult;
+    expectedResult.push_back(3);
+    expectedResult.push_back(4);
+    expectedResult.push_back(5);
+    expectedResult.push_back(7);
+    expectedResult.push_back(8);
+    expectedResult.push_back(10);
+    expectedResult.push_back(11);
+    expectedResult.push_back(14);
+    expectedResult.push_back(15);
+    expectedResult.push_back(16);
+    expectedResult.push_back(18);
+    std::vector<int> iteratedResult;
+
+    for(auto it = tree.begin(); it != tree.end(); it++) {
+        iteratedResult.push_back(*it);
+    }
+    assert(expectedResult.size() == iteratedResult.size());
+    for(int i=0;i<iteratedResult.size();i++) {
+        assert(iteratedResult[i] == expectedResult[i]);
+    }
+    return true;
+}
+
+bool TreeIterateSecondTest(){
+    std::cout<< "Iterating tree with end() method of the tree iterator:  ";
+    BTree<int> tree;
+	tree.addBOT(10);
+	tree.addBOT(7);
+	tree.addBOT(4);
+	tree.addBOT(8);
+	tree.addBOT(3);
+	tree.addBOT(5);
+    tree.addBOT(14);
+    tree.addBOT(11);
+    tree.addBOT(16);
+    tree.addBOT(15);
+    tree.addBOT(18);
+    std::vector<int> expectedResult;
+    expectedResult.push_back(3);
+    expectedResult.push_back(4);
+    expectedResult.push_back(5);
+    expectedResult.push_back(7);
+    expectedResult.push_back(8);
+    expectedResult.push_back(10);
+    expectedResult.push_back(11);
+    expectedResult.push_back(14);
+    expectedResult.push_back(15);
+    expectedResult.push_back(16);
+    expectedResult.push_back(18);
+    std::vector<int> iteratedResult;
+
+    for(auto it = tree.begin(); !it.end(); it++) {
+        iteratedResult.push_back(*it);
+    }
+    assert(expectedResult.size() == iteratedResult.size());
+    for(int i=0;i<iteratedResult.size();i++) {
+        assert(iteratedResult[i] == expectedResult[i]);
+    }
+    return true;
+}
 
 int main() {
     BTree<int> tree;
@@ -150,7 +231,10 @@ int main() {
     tree.addBOT(18);
 	tree.printLRoR();
 	std::cout << '\n';
-   for(auto it = tree.begin(); !it.end(); it++) {
-       std::cout<< *it<< " ";
-   }
+//    for(auto it = tree.begin(); it != tree.end(); it++) {
+//        std::cout<< *it<< "  ";
+//    }
+    std::cout << std::boolalpha << TreeIterateFirstTest() << std::endl;
+    std::cout << std::boolalpha << TreeIterateSecondTest() << std::endl;
+    return 0;
 }
